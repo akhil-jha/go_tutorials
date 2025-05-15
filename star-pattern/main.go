a9dfc01869f4cc8116f1cce76204173b1322c847
Akhil Jha <akjha@redhat.com>
2025-05-16 00:22:22 +0530

    rebase

--- star-pattern/main.go
+++ star-pattern/main.go
@@ -1,76 +1,64 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
 	var number = 5
 	// fmt.Println("Enter a number: ")
 	// fmt.Scan(&number)
 
 	/*
-			------------------------------------------------------------------------------------------
-			*
-			* *
-			* * *
-			* * * *
+		------------------------------------------------------------------------------------------
+		*
+		* *
+		* * *
+		* * * *
 
-			for i := 0; i < number; i++ {
-				for j := 0; j < i; j++ {
-					fmt.Printf("*")
-				}
-				fmt.Print("\n")
+		for i := 0; i < number; i++ {
+			for j := 0; j < i; j++ {
+				fmt.Printf("*")
 			}
-			------------------------------------------------------------------------------------------
-			* * * * *
-			* * * *
-			* * *
-			* *
-			*
+			fmt.Print("\n")
+		}
+		------------------------------------------------------------------------------------------
+		* * * * *
+		* * * *
+		* * *
+		* *
+		*
 
-			for i := 0; i < number; i++ {
-					for j := 0; j < (number - i); j++ {
-							print("*")
-					}
-					print("\n")
-			}
-			------------------------------------------------------------------------------------------
-			****
-			*  *
-			*  *
-			****
-
-			for i := 0; i < number; i++ {
-					for j := 0; j < number; j++ {
-							if i == 0 || i == number-1 || j == 0 || j == number-1 {
-									print("*")
-							} else {
-									print(" ")
-							}
-					}
-					print("\n")
-			}
-			------------------------------------------------------------------------------------------
-			****
-			 ****
-			  ****
-			   ****
-			   for i:=0 ; i < number ; i++ {
-					star := number
-					spaces := i
-					fmt.Printf("%s%s\n", strings.Repeat(" ", spaces), strings.Repeat("*", stars))
-			   }
-
-				for i := 0; i < number; i++ {
-					fmt.Printf("%s", strings.Repeat(" ", i))
-					for j := 0; j < number; j++ {
+		for i := 0; i < number; i++ {
+				for j := 0; j < (number - i); j++ {
 						print("*")
-					}
-					fmt.Printf("\n")
 				}
-			------------------------------------------------------------------------------------------
+				print("\n")
+		}
+		------------------------------------------------------------------------------------------
+		****
+		*  *
+		*  *
+		****
+
+		for i := 0; i < number; i++ {
+				for j := 0; j < number; j++ {
+						if i == 0 || i == number-1 || j == 0 || j == number-1 {
+								print("*")
+						} else {
+								print(" ")
+						}
+				}
+				print("\n")
+		}
+		------------------------------------------------------------------------------------------
+		****
+		 ****
+		  ****
+		   ****
+
+		for i:=0 ; i < number ; i++ {
+			star := number
+			spaces := i
+			fmt.Printf("%s%s\n", strings.Repeat(" ", spaces), strings.Repeat("*", stars))
+		}
+		------------------------------------------------------------------------------------------
 
 		   *
 		  ***
@@ -81,11 +69,7 @@ func main() {
 			stars := 2*i + 1
 			spaces := number - i - 1
 			fmt.Printf("%s%s\n", strings.Repeat(" ", spaces), strings.Repeat("*", stars))
-		}
-	*/
-	for i := 0; i < number; i++ {
-		stars := number
-		spaces := i
-		fmt.Printf("%s%s\n", strings.Repeat(" ", spaces), strings.Repeat("*", stars))
 	}
+	*/
+
 }
