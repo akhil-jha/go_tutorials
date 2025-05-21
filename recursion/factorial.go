af8243ce51aab1090e7481b9f79388d3322c6cc6
Akhil Jha <akjha@redhat.com>
2025-05-22 00:14:55 +0530

    Use uint64

--- recursion/factorial.go
+++ recursion/factorial.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 )
 
-func calculateFactorial(number int) int {
-	if number > 0 {
-		return number * calculateFactorial(number-1)
-	} else {
+func calculateFactorial(number uint64) uint64 {
+	if number == 0 {
 		return 1
 	}
+	return number * calculateFactorial(number-1)
 
 }
 
 func main() {
-	myNumber := 100
-	fmt.Printf("Factorial of %d is %d", myNumber, calculateFactorial(myNumber))
+	var myNumber uint64
+	myNumber = 65 // this is the max limit
+	fmt.Printf("Factorial of %d is %d\n", myNumber, calculateFactorial(myNumber))
 
 }
