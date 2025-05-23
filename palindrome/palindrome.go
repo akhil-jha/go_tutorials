45d53d1c136451adff700e2fa3800880ff8a1cca
Akhil Jha <akjha@redhat.com>
2025-05-24 01:48:15 +0530

    palindrome

---
+++ palindrome/palindrome.go
@@ -0,0 +1,55 @@
+/*
+Given an integer x, return true if x is a
+
+, and false otherwise.
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	if x < 10 {
+		return true
+	}
+	if x > 0 {
+		digitsArray := []int{}
+		i := 0
+		for x > 0 {
+			digitsArray = append(digitsArray, x%10)
+			x = x / 10
+			i++
+		}
+		/*
+			comparision loop here
+		*/
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	number := 121
+	fmt.Println(isPalindrome(number))
+}
