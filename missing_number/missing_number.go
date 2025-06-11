871960a87744085280b7aa4ba738de487fae91a7
Akhil Jha <akjha@redhat.com>
2025-06-11 13:28:14 +0530

    Missing number XOR

--- missing_number/missing_number.go
+++ missing_number/missing_number.go
@@ -1,52 +1,48 @@
 /*
-Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 
 
 Example 1:
 
-Input: nums = [3,0,1]
+Input: nums = [2,2,1]
 
-Output: 2
-
-Explanation:
-
-n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Output: 1
 
 Example 2:
 
-Input: nums = [0,1]
+Input: nums = [4,1,2,1,2]
 
-Output: 2
-
-Explanation:
-
-n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Output: 4
 
 Example 3:
 
-Input: nums = [9,6,4,2,3,5,7,0,1]
+Input: nums = [1]
 
-Output: 8
+Output: 1
 
-Explanation:
+Constraints:
 
-n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+    1 <= nums.length <= 3 * 104
+    -3 * 104 <= nums[i] <= 3 * 104
+    Each element in the array appears twice except for one element which appears only once.
 */
 
 package main
 
 import "fmt"
 
-func missingNumber(nums []int) int {
-	sum := 0
+func singleNumber(nums []int) int {
+	xor := 0
 	for i := 0; i < len(nums); i++ {
-		sum = sum + nums[i]
+		xor ^= nums[i]
 	}
-	return len(nums)*(len(nums)+1)/2 - sum
+	return xor
 }
 
 func main() {
-	nums := []int{9, 6, 4, 2, 3, 5, 7, 0, 1}
-	fmt.Println(missingNumber(nums))
+	nums := []int{4, 1, 2, 1, 2}
+	fmt.Println(singleNumber(nums))
 }
